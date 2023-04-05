4369715c4ff24f2934670d028cee6c30fdf42d59
Steven Vu <sssstevenvu@gmail.com>
2023-04-04 20:40:49 -0700

    Replaces svg files with png due to errors

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+// Big Illustrations
+export { default as hero_illustration } from './hero_illustration.png';
+
+// People
+export { default as child_boy } from './child_boy.png';
+export { default as child_girl } from './child_girl.png';
+export { default as man_laptop } from './man_laptop.png';
+export { default as woman_laptop } from './woman_laptop.png';
+
+// Props
+export { default as tulip } from './tulip.png';
+export { default as bench } from './bench.png';
+export { default as pin } from './pin.png';
