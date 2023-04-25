997eabbec74584bc34f4d13135618623c84f2cb5
Steven Vu <sssstevenvu@gmail.com>
2023-04-24 21:46:16 -0700

    fix: improved the css for the signup page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -33,71 +33,69 @@ const SignUp = () => {
     };
 
     return (
-        <div className ="pt-20 px-56">
-            <div className="absolute top-5 left-5">
-                <h1 className="block text-white text-lg font-bold">Employer<b className="text-purple-500">Eval</b></h1>
+        <div className ="min-h-screen flex flex-col">
+            <Link to='/'><h1 className="block text-white text-2xl font-bold px-8 py-4">Employer<span className="text-purple-500">Eval</span> <span className="text-base ml-3">Home</span> </h1></Link>
+            <div className ="flex flex-1 items-center px-36 gap-8 lg:px-8">
+                <div class="w-full max-w-md">
+                    <form className="black-gradient shadow-md rounded px-8 pt-6 pb-8 mb-4" id='sign-up-form' onSubmit={signUp}>
+                        <h1 className="block text-white-800 text-2xl font-bold mb-6">Create Your Account</h1> 
+                        <div className="mb-2">
+                            <label className="block white text-md font-bold" htmlFor='firstName'>First Name</label>
+                            <input className="shadow appearance-none bg-white border-2 border-purple-500 rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-none"
+                                name='firstName' 
+                                type='text'
+                                value={userInfo.firstName}
+                                onChange={(e) => inputChange(e)} 
+                            />
+                        </div>
+
+                        <div className="mb-2">
+                            <label className="block white text-md font-bold" htmlFor='lastName'>Last Name</label>
+                            <input className="shadow appearance-none bg-white border-2 border-purple-500 rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-none"
+                                name='lastName' 
+                                type='text' 
+                                value={userInfo.lastName}
+                                onChange={(e) => inputChange(e)} 
+                            />
+                        </div>
+                    
+                        <div className="mb-2">
+                            <label className="block white text-md font-bold" htmlFor='email'>Email</label>
+                            <input className="shadow appearance-none bg-white border-2 border-purple-500 rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-none"
+                                name='email' 
+                                type='email' 
+                                value={userInfo.email} 
+                                onChange={(e) => inputChange(e)} 
+                            />
+                        </div>
+
+                        <div className="mb-10 sm:mb-8">
+                            <label className="block white text-md font-bold" htmlFor='password'>Password</label>
+                            <input className="shadow appearance-none bg-white border-2 border-purple-500 rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-none"
+                                name='password' 
+                                type='password' 
+                                value={userInfo.password} 
+                                onChange={(e) => inputChange(e)} 
+                            />
+                        </div>
+
+                        <button type='submit' className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 rounded mb-4 w-full sm:mb-1">Sign Up</button>
+
+                        <div class="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+                            <h3 class="mx-4 text-center font-semibold dark:text-white text-xl">or</h3>
+                        </div>
+
+                        <div className="flex justify-center">
+                            <h4 className="text-white text-lg sm:text-base xsm:text-sm">Already have an account? 
+                                <Link className="text-purple-500 text-lg font-bold sm:text-base xsm:text-sm" to='/login'> LOGIN</Link>
+                            </h4>
+                        </div> 
+                    </form>
+                </div>
+                <h1 className="text-white text-6xl font-bold mb-auto w-[5%] mt-28 md:hidden">JOIN THE FAMILY!</h1>
+                <img src={bench} alt='A bench' className="absolute bottom-0 right-[354px] w-[236px] h-auto xl:right-[184px] xl:w-[136px]" />
+                <img src={tulip} alt='A tulip plant' className="absolute bottom-0 right-[130px] w-[183px] h-auto xl:right-[20px] xl:w-[200px] sm:w-[100px]" />
             </div>
-            <div class="w-full max-w-md">
-                <form className="black-gradient shadow-md rounded px-8 pt-6 pb-8 mb-4" id='sign-up-form' onSubmit={signUp}>
-                    <h1 className="flex justify-center block text-white-800 text-2xl font-bold mb-6">Create Your Account</h1> 
-                    <div className="mb-2">
-                        <label className="block white text-md font-bold" htmlFor='firstName'>First Name</label>
-                        <input className="shadow appearance-none bg-white border-2 border-purple-500 rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-none"
-                            name='firstName' 
-                            type='text'
-                            value={userInfo.firstName}
-                            onChange={(e) => inputChange(e)} 
-                        />
-                    </div>
-
-                    <div className="mb-2">
-                        <label className="block white text-md font-bold" htmlFor='lastName'>Last Name</label>
-                        <input className="shadow appearance-none bg-white border-2 border-purple-500 rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-none"
-                            name='lastName' 
-                            type='text' 
-                            value={userInfo.lastName}
-                            onChange={(e) => inputChange(e)} 
-                        />
-                    </div>
-                
-                    <div className="mb-2">
-                        <label className="block white text-md font-bold" htmlFor='email'>Email</label>
-                        <input className="shadow appearance-none bg-white border-2 border-purple-500 rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-none"
-                            name='email' 
-                            type='email' 
-                            value={userInfo.email} 
-                            onChange={(e) => inputChange(e)} 
-                        />
-                    </div>
-
-                    <div className="mb-10">
-                        <label className="block white text-md font-bold" htmlFor='password'>Password</label>
-                        <input className="shadow appearance-none bg-white border-2 border-purple-500 rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-none"
-                            name='password' 
-                            type='password' 
-                            value={userInfo.password} 
-                            onChange={(e) => inputChange(e)} 
-                        />
-                    </div>
-
-                    <div className="flex justify-center">
-                        <button type='submit' className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-40 rounded mb-4">Sign Up</button>
-                    </div>
-
-                    <div class="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
-                        <h3 class="mx-4 text-center font-semibold dark:text-white text-xl">or</h3>
-                    </div>
-
-                    <div className="flex justify-center">
-                        <h4 className="text-white text-lg">Already have an account? 
-                            <Link className="text-purple-500 text-lg font-bold" to='/login'> LOGIN</Link>
-                        </h4>
-                    </div> 
-                </form>
-            </div>
-            <h1 className="absolute top-56 right-44 block text-white text-5xl font-bold">JOIN THE FAMILY!</h1>
-            <img src={bench} alt='A bench' className="absolute bottom-0 right-[354px] w-[236px] h-auto md:right-[184px] md:w-[136px]" />
-            <img src={tulip} alt='A tulip plant' className="absolute bottom-0 right-[130px] w-[183px] h-auto md:right-[20px] md:w-[200px]" />
         </div>
     ) 
 }
