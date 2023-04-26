a79557231c7bebd5ac7d0b72576cd367bd4e3c0c
adenmengistie <adensm@icloud.com>
2023-04-26 08:08:01 -0700

    navigate to login page with signup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,11 +1,13 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { Link,  useNavigate} from "react-router-dom";
 import { useState } from "react";
 import { auth, createUser } from '../firebase';
 import {bench, tulip } from "../assets";
-import { Link } from "react-router-dom";
 
 const SignUp = () => {
-    // User Information
+    const navigate = useNavigate();
+
+    // User Informationnpm
     const [userInfo, setUserInfo] = useState({
         firstName: '',
         lastName: '',
@@ -21,11 +23,13 @@ const SignUp = () => {
     }
 
     // Form submission for Sign Up
-    const signUp = (e) => {
+    const signUp = async (e) => {
         e.preventDefault();
-        createUserWithEmailAndPassword(auth, userInfo.email, userInfo.password)
+
+        await createUserWithEmailAndPassword(auth, userInfo.email, userInfo.password)
             .then((userCredential) => {
                 createUser(userCredential.user.uid, userInfo);
+                navigate('/login')
             })
             .catch((err) => {
                 console.log(err);
@@ -38,7 +42,7 @@ const SignUp = () => {
                 <h1 className="block text-white text-lg font-bold">Employer<b className="text-purple-500">Eval</b></h1>
             </div>
             <div class="w-full max-w-md">
-                <form className="black-gradient shadow-md rounded px-8 pt-6 pb-8 mb-4" id='sign-up-form' onSubmit={signUp}>
+                <form className="black-gradient shadow-md rounded px-8 pt-6 pb-8 mb-4" id='sign-up-form'>
                     <h1 className="flex justify-center block text-white-800 text-2xl font-bold mb-6">Create Your Account</h1> 
                     <div className="mb-2">
                         <label className="block white text-md font-bold" htmlFor='firstName'>First Name</label>
@@ -81,7 +85,7 @@ const SignUp = () => {
                     </div>
 
                     <div className="flex justify-center">
-                        <button type='submit' className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-40 rounded mb-4">Sign Up</button>
+                        <button type='submit' onClick={signUp} className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-40 rounded mb-4">Sign Up</button>
                     </div>
 
                     <div class="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
@@ -90,7 +94,7 @@ const SignUp = () => {
 
                     <div className="flex justify-center">
                         <h4 className="text-white text-lg">Already have an account? 
-                            <Link className="text-purple-500 text-lg font-bold" to='/login'> LOGIN</Link>
+                            <Link className="text-purple-500 text-lg font-bold" to='/login'> SIGN IN</Link>
                         </h4>
                     </div> 
                 </form>
