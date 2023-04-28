f4c2f767108b3677a5e6038cb4a7825fd12ffa61
Haseeb <haseeb.sayed@sjsu.edu>
2023-04-27 18:02:12 -0700

    Account page, has the CSS done for the most part and minor functionalilty for now

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -47,7 +47,9 @@ const handleTabClick = (tabName) => {
 //   };
 
     return (
+    // First of three divs which divide the page into 3 columns, this first column contains the profile pic, username, profile, bookmarks etc 
     <div className="h-screen w-screen flex justify-between">    
+        {/* Div containing the profile pic, username, and name of user */}
         <div className=" w-96 ml-12 h-screen ">
             <div className="flex flex-col justify-between ">
             <img src={profilePic} alt="userImage" className="flex rounded-full mt-36 w-64 h-64 mx-16" />
@@ -55,11 +57,15 @@ const handleTabClick = (tabName) => {
                     <h2 className="ml-16 text-xl"> NAME HERE </h2>
                     <h2 className="ml-16"> username </h2>
                 </div>
+                
+                {/* Profile, bookmark, Reviews div */}
                 <div className=" mt-16">
                     <h2 className={`ml-16 py-2 text-xl ${activeTab === "profile" ? "underline" : "text-gray-400"} hover:underline cursor-pointer`} onClick={() => handleTabClick("profile")}> Profile </h2>
                     <h2 className={`ml-16 py-2 text-xl ${activeTab === "bookmarks" ? "underline" : "text-gray-400"} hover:underline cursor-pointer`} onClick={() => handleTabClick("bookmarks")}> Bookmarks </h2>
                     <h2 className={`ml-16 py-2 text-xl ${activeTab === "reviews" ? "underline" : "text-gray-400"} hover:underline cursor-pointer`} onClick={() => handleTabClick("reviews")}> Reviews </h2>
                 </div>
+                
+                {/* Two tulip images at the bottom of the screen */}
                 <div className="relative  mt-16 h-60">
                     <img src={tulip} alt="tulip" className="absolute bottom-0 right-0 mr-20 w-32 h-32 " />
                     <img src={tulip} alt="tulip" className="absolute bottom-0 w-48 h-48 opacity-30 " />
@@ -68,6 +74,7 @@ const handleTabClick = (tabName) => {
             </div>
         </div>
         
+        {/* Active tab functions to allow the rerendering of the page when a user clicks on profile, bookmarks, or reviews */}
         {activeTab === "profile" && 
             <div >
             
@@ -85,6 +92,7 @@ const handleTabClick = (tabName) => {
             </div>
         </div>}
         
+        {/* Second of the three column divs which divides the page, this div contains the main information such as profile information */}
         <div className=" flex flex-col relative w-1/3 ml-52 pt-32 gap-16 h-screen">
             <div className=" w-full h-1/2 blackFlip-gradient rounded-3xl ">
                 <h1 className="ml-6 pt-6 text-3xl"> Profile Details </h1>
@@ -99,6 +107,7 @@ const handleTabClick = (tabName) => {
             <div className=" w-full h-1/2 blackFlip-gradient rounded-3xl "></div>
         </div>
         
+        {/* Third column div which contains the two tulip images from the right of the screen */}
         <div className=" relative flex-1 ml-52">
             <img src={tulip} alt="tulip" className="absolute bottom-0 left-0 ml-16 mr-20 w-32 h-32 " />
             <img src={tulip} alt="tulip" className="absolute bottom-0 ml-36 w-48 h-48 opacity-30 " />
